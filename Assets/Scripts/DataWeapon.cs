59b909002f079e202eb515cbb0fb5d96dd5a29f4
KID <50666207+KID421@users.noreply.github.com>
2024-07-07 16:53:35 +0800

    Update 添加武器資料

--- Assets/Scripts/DataWeapon.cs
+++ Assets/Scripts/DataWeapon.cs
@@ -4,8 +4,26 @@ namespace KID
 {
     // MonoBehaviour 允許腳本掛在遊戲物件上
     // ScriptableObject 腳本化物件：將程式內容存放在專案內
+
+    // 建立素材自定義菜單 menuName = "主菜單/子菜單"
+    [CreateAssetMenu(menuName = "KID/Weapon")]
     public class DataWeapon : ScriptableObject
     {
-
+        [Header("彈匣裝彈量"), Range(0, 60)]
+        public int magazineBulletCount;
+        [Header("彈匣價格"), Range(0, 10000)]
+        public int magazinePrice;
+        [Header("子彈速度"), Range(0, 2000)]
+        public int bulletSpeed;
+        [Header("子彈生命"), Range(0, 2)]
+        public float bulletLift;
+        [Header("子彈傷害"), Range(0, 100)]
+        public float bulletDamage;
+        [Header("子彈後座力"), Range(0, 2)]
+        public float bulletRecoil;
+        [Header("子彈冷卻時間"), Range(0, 1)]
+        public float bulletCD;
+        [Header("彈匣冷卻時間"), Range(0, 2)]
+        public float magazineCD;
     }
 }
