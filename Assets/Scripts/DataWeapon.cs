cb9c7a5beaf1de7849731c4df9b7f3e04b1c6a02
KID <50666207+KID421@users.noreply.github.com>
2024-07-07 16:04:44 +0800

    Update 完成武器旋轉功能

---
+++ Assets/Scripts/DataWeapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    // MonoBehaviour 允許腳本掛在遊戲物件上
+    // ScriptableObject 腳本化物件：將程式內容存放在專案內
+    public class DataWeapon : ScriptableObject
+    {
+
+    }
+}
