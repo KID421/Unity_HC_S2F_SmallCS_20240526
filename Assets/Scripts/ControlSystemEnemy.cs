7632ea351f5add3048030c5887c03ed315083bfc
KID <50666207+KID421@users.noreply.github.com>
2024-08-18 12:25:00 +0800

    Update 添加敵人控制系統

---
+++ Assets/Scripts/ControlSystemEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 控制系統：敵人
+    /// </summary>
+    public class ControlSystemEnemy : ControlSystem
+    {
+        private Transform weaponFirePoint;
+
+        [Header("偵測玩家射線")]
+        [SerializeField]
+        private Color checkPlayerRayColor = new Color(0.5f, 1, 0.5f, 0.7f);
+        [SerializeField, Range(0, 15)]
+        private float checkPlayerLength = 3.5f;
+        [SerializeField]
+        private LayerMask checkPlayerLayer = 1 << 3 | 1 << 6;
+    }
+}
